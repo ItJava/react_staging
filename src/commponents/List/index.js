4a102ff8ac1c4eff3240c4a34a81f8bb93e52eaa
itjavawfc@163.com <itjavawfc@163.com>
2021-04-17 15:53:52 +0800

    兄弟节点之间 使用：PubSubJS  发布和订阅消息

--- src/commponents/List/index.js
+++ src/commponents/List/index.js
@@ -1,16 +1,31 @@
 import React, {Component} from 'react';
 import './index.css'
+import PubSub from 'pubsub-js'
 export default class List extends Component {
 
+    state={                    // 初始化状态，users 初始值为数组
+        users:[],
+        isFirst:true,          //是否第一次打开页面
+        isLoading:false,        //标识是否处于加载中
+        err:''                  //存储请求相关的错误信息
+    }
+
+    componentDidMount() {   //这个钩子 一般做初始化的工作
+        this.token=PubSub.subscribe('atguigu',(msg,data)=>{
+            console.log('List收到消息了,并打印出来消息',data)
+            this.setState(data)
+        })
+    }
+
+    componentWillUnmount() {
+        PubSub.unsubscribe(this.token)
+    }
 
 
     render() {
-
-        const  {users,isFirst,isLoading,err}=this.props
-
+        const  {users,isFirst,isLoading,err}=this.state
         console.log('users:',users.length)
         console.log('data:',isFirst+' '+isLoading+' '+err+' '+users.length)
-
         return (
                 <div className="row">
                     {
--- src/commponents/Search/index.js
+++ src/commponents/Search/index.js
@@ -1,28 +1,27 @@
 import React, {Component} from 'react';
 import axios from "axios";
+import PubSub from 'pubsub-js'
 
 export default class Search extends Component {
 
     search=()=>{
+
         //获取用户输入
-        //console.log(this.keyWorkElement.value)
         const {keyWorkElement:{value:keyWordValue}}=this   // 结构赋值 连着写 并且对对应的字段重新命令
         console.log(keyWordValue)
-        this.props.updateAppState({isFirst:false,isLoading:true})
-
+        PubSub.publish('atguigu',{isFirst:false,isLoading:true})
         //发送网络请求 //
         axios.get('http://localhost:3000/api1/search/users?q='+keyWordValue).then(
             response=>{
                console.log('成功了：',response.data)
-                 this.props.updateAppState({isLoading:false,err:false,users:response.data.items})
+                PubSub.publish('atguigu',{isLoading:false,err:false,users:response.data.items})
             },
             error=>{
                 console.log('失败了！')
-                 this.props.updateAppState({isLoading:false,err:error.message})
+                PubSub.publish('atguigu',{isLoading:false,err:error.message})
             }
         )
     }
-
 
     render() {
         return (
