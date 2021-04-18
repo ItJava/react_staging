10f52bf0ff56ec63c086f6428d137847f71782b1
wangfangchen <itjavawfc@163.com>
2021-04-18 11:30:23 +0800

    路由的基本使用

---
+++ 08_src_路由的基本使用/commponents/Home/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <h3>我是Home的内容</h3>
+        );
+    }
+}
+ 
